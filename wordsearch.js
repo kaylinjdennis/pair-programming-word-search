a5cebeaee112664820ab25bf6d9415336e407235
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-10 18:21:04 -0700

    Pair programming exercise to check if word is in wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair Programmed with Judit Méndez
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let array = [];
+  for (let row = 0; row < matrix[0].length; row++) {
+    let rowArray = [];
+    for (let column = 0; column < matrix.length; column++) {
+      rowArray.push(matrix[column][row]);
+    }
+    array.push(rowArray);
+  }
+  return array;
+};
+
+
+const wordSearch = (letters, word) => {
+  if (letters.length !== 0) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let line of horizontalJoin) {
+      if (line.includes(word)) {
+        return true;
+      }
+    }
+    const transposedLetters = transpose(letters);
+    const joinedLetters = transposedLetters.map(ls => ls.join(''));
+    for (let row of joinedLetters) {
+      if (row.includes(word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
